945bf6a74f580affe241d46fb68b6a5828c298f9
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-17 12:40:34 -0500

    0.3.2, helper commError fx added

--- server/helpers.js
+++ server/helpers.js
@@ -4,14 +4,25 @@ Server Helpers || Under the Seas App || Unit3 Assessment
 */
 
 
-const log = console.log;
-
 const pgp = require('pg-promise')();
     const connectString = 'postgres://localhost:5432/under_the_seas_db';
     const db = pgp(connectString);
 
 
+const log = console.log;
+
+const commError = (req, res, err, fxName) => { // handles errors communicating with database
+  log(`${fxName}: ${err}`);
+  res.status(err.status || 500);
+  res.json({
+      status: "fail",
+      message: "error: problem communicating with server. please try again later",
+      err: true
+  });
+}
+
 module.exports = {
+  db,
   log,
-  db
+  commError
 };
