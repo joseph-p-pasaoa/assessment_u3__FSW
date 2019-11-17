a77261a1a101219c265ba0de1fb7feeedffc7add
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-17 17:59:50 -0500

    0.7.2, index html init, js init

---
+++ client/js/index.js
@@ -0,0 +1,31 @@
+/*
+Joseph P. Pasaoa
+ClientSide Index JS | Under the Seas App | Unit3 Assessment
+*/
+
+
+/* HELPERS */
+const log = console.log;
+
+const serverComm = async (method, urlAdds, body) => {
+  const url = `http://localhost:11000/sightings/${urlAdds}`;
+  try {
+    const response = await axios[method](url, body);
+    return response.data;
+  } catch (err) {
+    log("client-side error: ", err);
+  }
+}
+
+const clearStage = () => {
+  const stage = document.querySelector('#stage');
+  while (stage.firstChild) {
+    stage.removeChild(stage.lastChild);
+  }
+}
+
+const grabAllSightings = async () => {
+  const researcherId = document.querySelector('#txtResearcher').value;
+  const response = await serverComm("get", `${researcherId}`);
+  return response.payload;
+}
