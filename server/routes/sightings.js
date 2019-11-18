eb2c3da7c36428ac962f2d00c39d293f48c42de2
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-17 19:59:49 -0500

    0.8.1, somehow was excluded in last commit

--- server/routes/sightings.js
+++ server/routes/sightings.js
@@ -22,7 +22,12 @@ DELETE /sightings/:id: Delete single sighting.
 */
 
 /* ROUTES */
-
+router.get('/', getSightings);
+// router.get('/:id', checkInputContent, getSightings);
+// router.get('/:id', checkInputContent, getSightings);
+// router.get('/:id', checkInputContent, getSightings);
+// router.post('/', checkInputsExist, checkInputContent, addSighting);
+// router.delete('/:id', checkIdExists, checkInputContent, delSighting);
 
 
 /* PRELIM MIDDLEWARE */
@@ -30,7 +35,38 @@ DELETE /sightings/:id: Delete single sighting.
 
 
 /* FINAL MIDDLEWARES */
-
+async function getSightings (req, res, next) {
+  let response = null;
+  let getQuery = `
+    SELECT *
+    FROM sightings
+  `;
+  let getArgs = null;
+  if (req.params.id) {
+    getQuery += ` WHERE id = $/id/`;
+    getArgs = { id: parseInt(req.params.id.trim()) };
+  }
+  getQuery += ` ORDER BY id ASC`;
+  try {
+    response = await db.any(getQuery, getArgs);
+  } catch (error) {
+    commError(req, res, error, "getSighting");
+  }
+  if (!response.length) {
+    res.status(404);
+    res.json({
+        status: "error",
+        message: "no sightings found",
+        payload: null
+    });
+  } else {
+    res.json({
+        status: "success",
+        message: response.length > 1 ? "sightings found" : "sighting found",
+        payload: response.length > 1 ? response : response[0]
+    });
+  }
+}
 
 
 module.exports = router;
